c6649c4fa4facc7bcb57e4ae56e2efc6d7f8de0d
muhammad-anas16 <anas.hanf38@gmail.com>
2024-10-11 19:20:51 +0500

    completed

--- app.js
+++ app.js
@@ -17,20 +17,3 @@ function getValue(value){
     }
 
 }
-
-// var num = 100;
-// console.log(`the result is: ${num+100} and the ana`) //template literals
-
-// var btn = `<button><i>submit</i></button>`
-// console.log(btn);
-// screenDiv.innerHTML = btn;
-
-
-
-// var array = [0,1,2,3,4]
-// var array2 = [0,1,2,3,4]
-// var array3 = [...array, ...array2] //spread operators
-//  array3 = [100, ...array3]
-
-
-// console.log(array3);
